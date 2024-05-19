715bafc9d5eb91769e6cc6a3af1b54028b2679f6
firdausmuhammad63 <daoes700@gmail.com>
2024-05-19 17:29:24 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
+
 // ketika hamburger menu di-klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
@@ -15,11 +16,16 @@ document.querySelector("#search-button").onclick = (e) => {
   e.prevenDefault();
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
